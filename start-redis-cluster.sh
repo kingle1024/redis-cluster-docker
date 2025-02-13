30d4d0fda51fd3708cc3f025f93202bb233d000d
kingle1024 <teran1024@naver.com>
2025-02-13 22:49:30 +0900

    Redis 레플리카 인스턴스 실행 부분 제거
    레플리카 설정 제거: replicaof 지시어를 사용하지 않고, 클러스터 생성 시 --cluster-replicas 옵션을 통해 레플리카를 지정해야 함.
    
    레플리카 노드의 실행: 레플리카 노드는 별도로 실행할 필요 없이, 마스터 노드와 함께 클러스터 구성 시 자동으로 생성됨.

--- start-redis-cluster.sh
+++ start-redis-cluster.sh
@@ -5,11 +5,6 @@ redis-server --port 6379 --cluster-enabled yes --cluster-config-file /data/nodes
 redis-server --port 6380 --cluster-enabled yes --cluster-config-file /data/nodes-6380.conf --cluster-node-timeout 5000 &
 redis-server --port 6381 --cluster-enabled yes --cluster-config-file /data/nodes-6381.conf --cluster-node-timeout 5000 &
 
-# Redis 인스턴스 실행 (레플리카)
-redis-server --port 6382 --cluster-enabled yes --cluster-config-file /data/nodes-6382.conf --cluster-node-timeout 5000 --slaveof 127.0.0.1 6379 &
-redis-server --port 6383 --cluster-enabled yes --cluster-config-file /data/nodes-6383.conf --cluster-node-timeout 5000 --slaveof 127.0.0.1 6380 &
-redis-server --port 6384 --cluster-enabled yes --cluster-config-file /data/nodes-6384.conf --cluster-node-timeout 5000 --slaveof 127.0.0.1 6381 &
-
 # 모든 Redis 인스턴스가 실행될 때까지 대기
 sleep 5
 
