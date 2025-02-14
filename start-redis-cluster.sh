0f85530d970a7b3e3d11c27d86583f54f3dc844c
kingle1024 <teran1024@naver.com>
2025-02-14 15:20:33 +0900

    레플리카 임시 주석 처리

--- start-redis-cluster.sh
+++ start-redis-cluster.sh
@@ -6,9 +6,9 @@ for port in 6379 6380 6381; do
 done
 
 # Redis 인스턴스 실행 (레플리카)
-for port in 6382 6383 6384; do
-  redis-server --port $port --cluster-enabled yes --cluster-config-file /data/nodes-$port.conf --cluster-node-timeout 5000 &
-done
+# for port in 6382 6383 6384; do
+#   redis-server --port $port --cluster-enabled yes --cluster-config-file /data/nodes-$port.conf --cluster-node-timeout 5000 &
+# done
 
 # 모든 Redis 인스턴스가 실행될 때까지 대기
 sleep 5
@@ -18,10 +18,10 @@ redis-cli --cluster create \
 127.0.0.1:6379 \
 127.0.0.1:6380 \
 127.0.0.1:6381 \
-127.0.0.1:6382 \
-127.0.0.1:6383 \
-127.0.0.1:6384 \
---cluster-replicas 1
+# 127.0.0.1:6382 \
+# 127.0.0.1:6383 \
+# 127.0.0.1:6384 \
+--cluster-replicas 0
 
 # 모든 Redis 인스턴스가 실행될 때까지 대기
 wait
